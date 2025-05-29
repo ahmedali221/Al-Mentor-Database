83fe16a2d0a2f3a4ef0a88c1d683b70947e79c25
Ahmed Ali <113573377+ahmedali221@users.noreply.github.com>
2025-05-29 15:29:18 +0300

    updated server location

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
 const express = require("express");
 const http = require("http");
-const connectDB = require("./db");
-const initializeWebSocketServer = require("../websocket/chatServer");
+const connectDB = require("./config/db");
+const initializeWebSocketServer = require("./websocket/chatServer");
 // Users Routes
-const userRoutes = require("../routes/Users/userRoute");
-const instructorRoutes = require("../routes/Users/instructorRoutes");
-const userSavedCourseRoutes = require("../routes/Users/userSavedCourseRoutes");
-const chatRoutes = require("../routes/Users/chatRoutes");
-const instructorSessionRoutes = require("../routes/Users/instructorSessionRoutes");
+const userRoutes = require("./routes/Users/userRoute");
+const instructorRoutes = require("./routes/Users/instructorRoutes");
+const userSavedCourseRoutes = require("./routes/Users/userSavedCourseRoutes");
+const chatRoutes = require("./routes/Users/chatRoutes");
+const instructorSessionRoutes = require("./routes/Users/instructorSessionRoutes");
 
 // Courses Routes
-const courseRoutes = require("../routes/Topics & Courses/courseRoutes");
-const lessonRoutes = require("../routes/Topics & Courses/lessonRoutes");
-const moduleRoutes = require("../routes/Topics & Courses/moduleRoutes");
-const topicRoutes = require("../routes/Topics & Courses/topicRoutes");
-const categoryRoutes = require("../routes/Topics & Courses/categoryRoutes");
-const subTopicRoutes = require("../routes/Topics & Courses/subTopicRoutes");
-const favoriteLessonRoutes = require("../routes/Topics & Courses/favoriteLessonRoutes");
+const courseRoutes = require("./routes/Topics & Courses/courseRoutes");
+const lessonRoutes = require("./routes/Topics & Courses/lessonRoutes");
+const moduleRoutes = require("./routes/Topics & Courses/moduleRoutes");
+const topicRoutes = require("./routes/Topics & Courses/topicRoutes");
+const categoryRoutes = require("./routes/Topics & Courses/categoryRoutes");
+const subTopicRoutes = require("./routes/Topics & Courses/subTopicRoutes");
+const favoriteLessonRoutes = require("./routes/Topics & Courses/favoriteLessonRoutes");
 
 // Auth Route
-const authRoutes = require("../routes/Users/authRoutes");
+const authRoutes = require("./routes/Users/authRoutes");
 
 // Enrollment Routes
-const enrollmentRoutes = require("../routes/Subscription & Payment/EnrollmentRoutes");
-const paymentRoutes = require("../routes/Subscription & Payment/PaymentRoutes");
-const subscriptionRoutes = require("../routes/Subscription & Payment/subscriptionRoutes");
-const userSubscriptionRoutes = require("../routes/Subscription & Payment/userSubscriptionRoutes");
-const stripeRoutes = require("../routes/Subscription & Payment/stripeRoute");
-const webhookRoutes = require("../routes/Subscription & Payment/Webhook");
+const enrollmentRoutes = require("./routes/Subscription & Payment/EnrollmentRoutes");
+const paymentRoutes = require("./routes/Subscription & Payment/PaymentRoutes");
+const subscriptionRoutes = require("./routes/Subscription & Payment/subscriptionRoutes");
+const userSubscriptionRoutes = require("./routes/Subscription & Payment/userSubscriptionRoutes");
+const stripeRoutes = require("./routes/Subscription & Payment/stripeRoute");
+const webhookRoutes = require("./routes/Subscription & Payment/Webhook");
 
 // Programs Routes
-const programRoutes = require("../routes/Programs/programRoute");
-const programCourseRoutes = require("../routes/Programs/programCourseRoute");
-const userProgramProgressRoutes = require("../routes/Programs/userProgramProgressRoute");
+const programRoutes = require("./routes/Programs/programRoute");
+const programCourseRoutes = require("./routes/Programs/programCourseRoute");
+const userProgramProgressRoutes = require("./routes/Programs/userProgramProgressRoute");
 
 const cors = require("cors");
 const app = express();
